e00aa8db48c522fc167124021ce31e0d7d81c1f6
SandunMadhura <smadhurageeth@gmail.com>
2025-03-24 22:30:17 +0530

    create studetails.java file

---
+++ studetails.java
@@ -0,0 +1,16 @@
+public class studetails {
+    public static void main(String[] args) {
+        String stuName = "Sandun Madhura";
+        int stuId = 0001;
+        int stuAge = 20;
+        float stuFee = 68.5f;
+        char stuGrade = 'B';
+
+        //print variables
+        System.out.println("Student Name:" + stuName);
+        System.out.println("Student Id:" + stuId);
+        System.out.println("Student Age:" + stuAge);
+        System.out.println("Student Fee:" + stuFee);
+        System.out.println("Student Grade:" + stuGrade);
+    }
+}
