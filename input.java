740617dea8d7f94d6ea63c6dc3c8d7dd8110de94
SandunMadhura <smadhurageeth@gmail.com>
2025-03-25 23:02:16 +0530

    creat an input.java file

---
+++ input.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class input {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+            System.out.println("Enter Your Name");
+            String stuName = myObj.nextLine();
+            
+
+            System.out.println("Enter Your Age");
+            int age = myObj.nextInt();
+            
+
+            System.out.println("Enter Your City");
+            String city = myObj.nextLine();
+
+                System.out.println("Name : "+stuName);
+                System.out.println("Age : "+age);
+                System.out.println("City : "+city);
+    }
+}
